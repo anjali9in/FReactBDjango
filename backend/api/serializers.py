fe47cc1957628955794a284c142e7281922c362d
anjali21i <anjaliguptajnp21@gmail.com>
2025-12-18 01:26:16 +0530

    Stop tracking virtualenv (env/) and ignore it

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,3 +2,21 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    # old way and unsafe
+    # def create(self, validated_data):
+    #     user = User(
+    #         username=validated_data['username']
+    #     )
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #     return user
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
